1724774520254d0461b6ceda89cd2246e3d7af2a
Charles Zhu <zhuq1@vmware.com>
2016-08-10 11:03:15 -0700

    Aux scripts

--- aux/liotad.py
+++ aux/liotad.py
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # ----------------------------------------------------------------------------#
 #  Copyright © 2015-2016 VMware, Inc. All Rights Reserved.                    #
@@ -31,37 +31,4 @@
 #  THE POSSIBILITY OF SUCH DAMAGE.                                            #
 # ----------------------------------------------------------------------------#
 
-liota_config="/etc/liota/conf/liota.conf"
-package_messenger_pipe=""
-
-if [ ! -f "$liota_config" ]; then
-	echo "ERROR: Configuration file not found" >&2
-	exit -1
-fi
-
-while read line # Read configurations from file
-do
-	if echo $line | grep -F = &>/dev/null
-	then
-		varname=$(echo "$line" | sed "s/^\(..*\)\s*\=\s*..*$/\1/")
-		if [ $varname == "pkg_msg_pipe" ]; then
-	    	value=$(echo "$line" | sed "s/^..*\s*\=\s*\(..*\)$/\1/")
-			package_messenger_pipe=$value
-		fi
-	fi
-done < $liota_config
-
-if [ "$package_messenger_pipe" == "" ]; then
-	echo "ERROR: Pipe path not found in configuration file" >&2
-	exit -2
-fi
-
-if [ ! -p "$package_messenger_pipe" ]; then
-	echo "ERROR: Pipe path is not a named pipe" >&2
-	exit -3
-fi
-
-# Echo to named pipe
-echo "Pipe file: $package_messenger_pipe" >&2
-echo "$@" > $package_messenger_pipe
-
+import liota.core.package_manager
